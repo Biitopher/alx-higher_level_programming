871efe3bca0118d90e4b609467c56089ef9e27d0
Biitopher <biitopher@gmail.com>
2023-06-06 13:18:38 -0700

    9-print_last_digit.py

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -1,7 +1,4 @@
 #!/usr/bin/python3
 def print_last_digit(number):
-    """Print the last digit of a number."""
-
-
-last_digit = number % 10
-print(last_digit)
+    last_digit = number % 10
+    print(last_digit)
