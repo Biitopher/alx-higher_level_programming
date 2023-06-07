b4bd98eb7e55fba404a2c4e4eeaa3231cdac569a
Biitopher <biitopher@gmail.com>
2023-06-07 05:30:03 -0700

    12-fizzbuzz.py

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -3,7 +3,7 @@ def fizzbizz():
     for x in range(1, 101):
         if x % 3 == 0 and x % 5 == 0:
             print("FizzBuzz ", end="")
-            elif x % 3 == 0:
+            if x % 3 == 0:
                 print("Fizz ", end="")
             elif x % 5 == 0:
                 print("Buzz ", end="")
