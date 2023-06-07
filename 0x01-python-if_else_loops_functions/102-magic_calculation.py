6577134238ce6e47c10b68b92699cc7986314920
Biitopher <biitopher@gmail.com>
2023-06-07 12:13:09 -0700

    102-magic_calculation.py

--- 0x01-python-if_else_loops_functions/102-magic_calculation.py
+++ 0x01-python-if_else_loops_functions/102-magic_calculation.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
+ #!/usr/bin/python3
 def magic_calculation(a, b, c):
     if a < b:
-        return a + b + c
-    elif a > b and c != 0:
-        return (a - b) / c
-    else:
-        return a * b - c
+        return (c)
+    if c > b:
+        return (a + b)
+    return (a * b - c)
+
