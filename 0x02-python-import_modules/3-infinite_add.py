0cd2fa33b185a970fb89e3bb3346898b1d1183c2
Biitopher <biitopher@gmail.com>
2023-06-08 13:31:00 -0700

    3-infinite_add.py

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     import sys
-    result = 0
-    for x in range(len(sys.argv) - 1):
-        result += int(sys.argv[x + 1])
-        print("{}".format(result))
+    args = sys.argv[1:]
+    result = sum(int(arg) for arg in args)
+    print(result)
