e85939df4d0ee30049dc8d0350a48993238cf221
Biitopher <biitopher@gmail.com>
2023-06-09 13:39:33 -0700

    102-magic_calculation.py

--- 0x02-python-import_modules/102-magic_calculation.py
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+import dis
 def magic_calculation(a, b):
     from magic_calculation_102 import add, sub
     if a < b:
@@ -8,3 +9,4 @@ def magic_calculation(a, b):
             return (c)
         else:
             return (sub(a, b))
+        print(dis.dis(magic_calculation))
