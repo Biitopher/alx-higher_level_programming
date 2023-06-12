73bb8f5cf99b7ed71f1a34d640b118c9b82b117f
Biitopher <biitopher@gmail.com>
2023-06-12 08:13:09 -0700

    11-delete_at.py

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,7 +1,11 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
-    if matrix:
-        for row in matrix:
-            for col in row:
-                print("{:d}".format(col), end=" ")
-                print()
+    if matrix is None or len(matrix) == 0:
+        print()
+        return None
+    if len(matrix) == 1 or len(matrix[0]) == 0:
+        print()
+        return None
+    for row in matrix:
+        for item in row:
+            print("{:d}".format(item),end="") 
