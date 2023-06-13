ddc79ac9602baeadf0d4df004afbb48aa8f7fe7d
Biitopher <biitopher@gmail.com>
2023-06-13 15:41:49 -0700

    102-magic_calculation.py

--- 0x02-python-import_modules/102-magic_calculation.py
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -4,6 +4,9 @@ import dis
 
 def magic_calculation(a, b):
     from magic_calculation_102 import add, sub
+
+    c = 0
+    add, sub = magic_calculation_102.add, magic_calculation_102.sub
     if a < b:
         c = add(a, b)
         for x in range(4, 6):
