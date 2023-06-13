fc0024126d5f983e585b7caee63675d0e74193ea
Biitopher <biitopher@gmail.com>
2023-06-13 07:17:54 -0700

    100-print_python_list_info.c

--- 0x03-python-data_structures/100-print_python_list_info.c
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -1,8 +1,8 @@
-#include <stdio.h>
+#include <python.h>
 
 /*
  * print_python_list_info - prints python list info
- * @p: list to be printed
+ * @p: list parameter to be printed
  * Return: Nothing
  */
 
@@ -12,6 +12,7 @@ Py_ssize_t size, i;
 PyObject *item;
 size = PyList_Size(p);
 Py_ssize_t allocated = ((PyListObject *)p)->allocated;
+item = PyList_GetItem(p, i);
 
 printf("[*] Size of the Python List = %ld\n", size);
 printf("[*] Allocated = %ld\n", allocated);
@@ -21,7 +22,6 @@ printf("       Address: %p\n", (void *)PyList_GetItem(p, 0));
 
 for (i = 1; i < size; i++)
 {
-item = PyList_GetItem(p, i);
 printf("[*] Element %ld: %s\n", i, Py_TYPE(item)->tp_name);
 printf("       Address: %p\n", (void *)item);
 }
