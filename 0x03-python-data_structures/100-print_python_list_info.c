0c8655fc00d86687c0bc62f6e72302b53393a40f
Biitopher <biitopher@gmail.com>
2023-06-13 10:41:44 -0700

    100-print_python_list_info.c

--- 0x03-python-data_structures/100-print_python_list_info.c
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -1,22 +1,22 @@
-#include <python.h>
+#include <Python.h>
 
-/*
- * print_python_list_info - prints python list info
- * @p: list parameter to be printed
- * Return: Nothing
- */
+/**
+* print_python_list_info - prints python list info
+* @p: list parameter to be printed
+*
+* Return: Nothing
+*/
 
 void print_python_list_info(PyObject *p)
 {
-Py_ssize_t x;
-size = PyList_Size(p);
-Py_ssize_t allocated = ((PyListObject *)p)->allocated;
-item = PyList_GetItem(p, x);
+	Py_ssize_t  = PyList_Size(p);
+	Py_ssize_t allocated = ((PyListObject *)p)->allocated;
+	PyObject *item = PyList_GetItem(p, x);
 
-printf("[*] Size of the Python List = %ld\n", size);
-printf("[*] Allocated = %ld\n", Py_ssize_t allocated);
-
-for (x = 1; x < size; x++)
-{
-printf("[*] Element %ld: %s\n", x, Py_TYPE(item)->tp_name);
+	printf("[*] Size of the Python List = %ld\n", size);
+	printf("[*] Allocated = %ld\n", allocated);
+	for (x = 0; x < size; x++)
+	{
+		printf("Element %ld: %s\n", x, Py_TYPE(item)->tp_name);
+	}
 }
