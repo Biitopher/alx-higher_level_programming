cddd1eb69116972d69ec82b841823f26a40b8a45
Biitopher <biitopher@gmail.com>
2023-06-14 12:25:01 -0700

    6-print_sorted_dictionary.py

---
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+def print_sorted_dictionary(a_dictionary):
+    order_list = list(a_dictionary.keys())
+    order_list.sort()
+    for x in order_list:
+        print("{}: {}".format(x, a_dictionary.get(x)))    
