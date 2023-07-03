3fef321a42f23d8898f4e4f56a53e3bb4aa2659e
Biitopher <biitopher@gmail.com>
2023-07-03 06:45:24 -0700

    3-rectangle.py

--- 0x08-python-more_classes/3-rectangle.py
+++ 0x08-python-more_classes/3-rectangle.py
@@ -4,6 +4,10 @@
 
 class Rectangle:
     """"Represents rectangle"""
+    def __init__(self, width=0, height=0):
+        self.width = width
+        self.height = height
+
     @property
     def width(self):
         return self.__width
