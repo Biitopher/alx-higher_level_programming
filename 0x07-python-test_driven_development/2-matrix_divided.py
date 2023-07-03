9fcfbd5129ca5ad4b770a2c33cff09206731e474
Biitopher <biitopher@gmail.com>
2023-07-03 15:54:24 -0700

    2-matrix_divided.py

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -2,13 +2,22 @@
 
 def matrix_divided(matrix, div):
     if not all(isinstance(row, list) for row in matrix) or \
-            not all(all(isinstance(element, (int, float)) for element in row) for row in matrix):
-                raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
-            if (len(set(len(row) for row in matrix)) > 1):
-                raise TypeError("Each row of the matrix must have the same size")
-            if not isinstance(div, (int, float)):
-                raise TypeError("div must be a number")
-            if div == 0:
-                raise ZeroDivisionError("division by zero")
-            new_matrix = [[round(element / div, 2) for element in row] for row in matrix]
-            return new_matrix
+            not all(isinstance(num, (int, float)) for row in matrix for num in row):
+        raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+
+    row_size = len(matrix[0])
+    if not all(len(row) == row_size for row in matrix):
+        raise TypeError("Each row of the matrix must have the same size")
+
+    if not isinstance(div, (int, float)):
+        raise TypeError("div must be a number")
+
+    if div == 0:
+        raise ZeroDivisionError("division by zero")
+
+    divided_matrix = []
+    for row in matrix:
+        divided_row = [round(num / div, 2) for num in row]
+        divided_matrix.append(divided_row)
+
+    return divided_matrix
