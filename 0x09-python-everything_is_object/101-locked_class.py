056af411aa682e93e92630ab47d26887ad554867
Biitopher <biitopher@gmail.com>
2023-07-04 04:42:57 -0700

    101-locked_class.py

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -5,6 +5,7 @@
 class LockedClass:
     __slots__ = ['first_name']
 
+
 def __setattr__(self, name, value):
     if not hasattr(self, name) and name != "first_name":
         raise AttributeError("Cannot create new instance attributes.")
