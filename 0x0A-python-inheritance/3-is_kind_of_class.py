0236d9fbaf9585015e3b28c5392bbff0bed34f1f
Biitopher <biitopher@gmail.com>
2023-07-10 04:25:37 -0700

    3-is_kind_of_class.py

---
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Defines class function"""
+
+
+def is_kind_of_class(obj, a_class):
+    """Represents kind of class"""
+    if isinstance(obj, a_class):
+        return True
+    return False
+
