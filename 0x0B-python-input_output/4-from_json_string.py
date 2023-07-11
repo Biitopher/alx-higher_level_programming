943d8f045e6fe2b489bc91584c381a5aa250fbe5
Biitopher <biitopher@gmail.com>
2023-07-11 03:56:55 -0700

    4-from_json_string.py

---
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Defines function that returns an object"""
+
+
+import json
+
+
+def from_json_string(my_str):
+    """Represents json string"""
+    return json.loads(my_str)
