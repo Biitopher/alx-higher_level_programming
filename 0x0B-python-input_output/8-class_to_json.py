ff40c55ee88abae07a6dd68c419a92c5fcc9ebae
Biitopher <biitopher@gmail.com>
2023-07-11 09:42:45 -0700

    8-class_to_json.py

--- 0x0B-python-input_output/8-class_to_json.py
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -4,15 +4,4 @@
 
 def class_to_json(obj):
     """Represents class to json"""
-    if isinstance(obj, (list, tuple)):
-        return [class_to_json(item) for item in obj]
-    elif isinstance(obj, dict):
-        return {class_to_json(key): class_to_json(value)
-                for key, value in obj.items()}
-    elif isinstance(obj, str):
-        return obj
-    elif isinstance(obj, (int, float, bool)):
-        return obj
-    else:
-        return {key: class_to_json(getattr(obj, key))
-                for key in dir(obj) if not key.startswith('__')}
+    return obj.__dict__
