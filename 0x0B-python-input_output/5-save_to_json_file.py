e922046829563179548642806c0f043b4a69df91
Biitopher <biitopher@gmail.com>
2023-07-11 04:05:21 -0700

    5-save_to_json_file.py

--- 0x0B-python-input_output/5-save_to_json_file.py
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""Defines function that writes an Object to a text file, using a JSON representation"""
+"""Defines function that writes an Object to a text file"""
 
 
 import json
