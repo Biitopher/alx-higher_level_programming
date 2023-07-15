2cb456aaeec035634faf3ca508de1dc118169c3d
biitopher <biitopher@gmail.com>
2023-07-15 13:03:12 -0700

    square.py

---
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""Defines square"""
+
+
+from models.rectangle import Rectangle
+
+
+class Square(Rectangle):
+    """Represents square"""
+    def __init__(self, size, x=0, y=0, id=None):
+        super().__init__(size, size, x, y, id)
+
+    @property
+    def size(self):
+        return self.width
+
+    @size.setter
+    def size(self, value):
+        self.width = value
+        self.height = value
+
+    def __str__(self):
+        return f"[Square] ({self.id}) {self.x}/{self.y} - {self.width}"
+
+    def update(self, *args, **kwargs):
+        if args:
+            arg_names = ['id', 'size', 'x', 'y']
+            for a, arg in enumerate(args):
+                setattr(self, arg_names[a], arg)
+        elif kwargs:
+            for key, value in kwargs.items():
+                setattr(self, key, value)
