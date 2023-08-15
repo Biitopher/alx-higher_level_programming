aafff72dec70db504e0b4172c806e18bd26b7472
biitopher <biitopher@gmail.com>
2023-08-15 15:29:23 -0700

    [Fix]Temperatures

--- 0x0D-SQL_introduction/102-top_city.sql
+++ 0x0D-SQL_introduction/102-top_city.sql
@@ -1,6 +1,6 @@
 -- Import database in table dump
-SELECT city, AVG(value) AS avg_temperature
+SELECT city, AVG(value) AS avg_temp
 FROM temperatures 
 WHERE month  BETWEEN ('7' AND '8')
 GROUP BY city
-ORDER BY avg_temperature DESC LIMIT 3;
+ORDER BY avg_temp DESC LIMIT 3;
