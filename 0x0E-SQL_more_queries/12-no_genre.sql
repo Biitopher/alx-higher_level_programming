ae9b1aacf698abc4f35cb6d65821aaba052083ac
biitopher <biitopher@gmail.com>
2023-08-16 15:02:39 -0700

    Task 12

--- 0x0E-SQL_more_queries/12-no_genre.sql
+++ 0x0E-SQL_more_queries/12-no_genre.sql
@@ -1,6 +1,6 @@
 -- List all shows without genre linked
-SELECT title AS title, tv_show_genres.genre_id AS genre_id
+SELECT title, tv_show_genres.genre_id
 FROM tv_shows
-LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
-WHERE genre_id is NULL
-ORDER BY title, genre_id ASC;
+LEFT JOIN tv_show_genres ON id=tv_show_genres.show_id
+WHERE tv_show_genres.show_id IS NULL
+ORDER BY title, tv_show_genres.genre_id;
