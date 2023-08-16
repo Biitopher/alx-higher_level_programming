2754c7f67f946dece65c69037cba329057b4062c
biitopher <biitopher@gmail.com>
2023-08-16 05:25:31 -0700

    Task 6

--- 0x0E-SQL_more_queries/6-states.sql
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -1,6 +1,7 @@
 -- Create database hbtn_0d_usa and the table states
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
-CREATE TABLE IF NOT EXISTS htbn_0d_usa.states (
-	id INT UNIQUE NOT NULL AUTO_INCREMENT PRIMARY KEY,
+USE hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS states (
+	id INT UNIQUE AUTO_INCREMENT NOT NULL PRIMARY KEY,
 	name VARCHAR(256) NOT NULL
 );
