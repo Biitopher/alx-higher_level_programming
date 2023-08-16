c657d1abbd5f27faed620e92a7ffa812ae639470
biitopher <biitopher@gmail.com>
2023-08-16 15:18:47 -0700

    Task 19

--- 0x0E-SQL_more_queries/102-rating_shows.sql
+++ 0x0E-SQL_more_queries/102-rating_shows.sql
@@ -1,5 +1,7 @@
 -- Lists shows by their rating
-SELECT title, SUM(rating) AS rating_sum
-FROM hbtn_0d_tvshows_rate
-GROUP BY title
+SELECT tv_shows.title, SUM(tvshow_ratings.rating) AS rating_sum
+FROM tv_shows
+JOIN tvshow_ratings ON tv_shows.id = tvshow_ratings.tv_show_id
+GROUP BY tv_shows.title
 ORDER BY rating_sum DESC;
+
