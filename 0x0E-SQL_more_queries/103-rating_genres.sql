b2baba9cef438806360813ce51075f58f5329d85
biitopher <biitopher@gmail.com>
2023-08-16 16:10:26 -0700

    Task 20

--- 0x0E-SQL_more_queries/103-rating_genres.sql
+++ 0x0E-SQL_more_queries/103-rating_genres.sql
@@ -1,7 +1,9 @@
 -- Lists all genres in database by their rating
-SELECT g.name AS genre_name, SUM(s.rating) AS rating_sum
-FROM tv_genres g
-JOIN tv_shows_genres sg ON g.id = sg.genre_id
-JOIN tv_shows s ON sg.show_id = s.id
-GROUP BY g.name
-ORDER BY rating_sum DESC;
+SELECT tv_genres.name,
+SUM(tv_show_ratings.rate) AS rating
+FROM tv_genres
+JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
+JOIN tv_shows ON tv_show_genres.show_id = tv_shows.id
+JOIN tv_show_ratings ON tv_show_ratings.show_id = tv_shows.id
+GROUP BY tv_genres.name
+ORDER BY rating DESC;
