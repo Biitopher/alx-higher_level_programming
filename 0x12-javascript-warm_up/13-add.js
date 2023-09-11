174c46fa5f9ede96ec37c7f8d7e0bcd3138c27ff
Biitopher <biitopher@gmail.com>
2023-09-11 06:29:39 -0700

    Task 13

--- 0x12-javascript-warm_up/13-add.js
+++ 0x12-javascript-warm_up/13-add.js
@@ -1,7 +1,9 @@
 #!/usr/bin/node
 
-const calculator = add: function(a, b) {
-    return a + b;
-}
+const calculator = {
+  add: function(a, b) {
+    return (a + b);
+  }
+};
 
 module.exports = calculator;
