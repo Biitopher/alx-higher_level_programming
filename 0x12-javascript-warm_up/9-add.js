35129647d5cada65723cebf1011d402143419e4e
Biitopher <biitopher@gmail.com>
2023-09-11 05:06:35 -0700

    Task 9

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -1,12 +1,14 @@
 #!/usr/bin/node
 
-function add(a, b)
-{
+function add(a, b) {
 	return a + b;
 }
 
 const arg1 = process.argv[2];
 const arg2 = process.argv[3];
 
-const result = add(arg1, arg2);
+const num1 = parseInt(arg1);
+const num2 = parseInt(arg2);
+
+const result = add(num1, num2);
 console.log(result);
