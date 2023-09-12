32c7b9f1015914656f14e02039b200eb178893e9
Biitopher <biitopher@gmail.com>
2023-09-12 08:42:38 -0700

    Task 12

---
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -0,0 +1,18 @@
+#!/usr/bin/node
+
+const { dict } = require('./101-data');
+
+const usersByOccurrence = {};
+
+for (const userId in dict) {
+  const occurrences = dict[userId];
+
+  if (!usersByOccurrence[occurrences]) {
+    usersByOccurrence[occurrences] = [];
+  }
+
+  usersByOccurrence[occurrences].push(userId);
+}
+
+console.log(usersByOccurrence);
+
