c8e0f927f234ec3493eb715e66548534f2dd4b34
Biitopher <biitopher@gmail.com>
2023-09-12 05:26:07 -0700

    Task 3

---
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -0,0 +1,23 @@
+#!/usr/bin/node
+
+class Rectangle {
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+}
+
+print() {
+    if (this.width === 0 || this.height === 0) {
+      console.log();
+    } else {
+      for (let x = 0; x < this.height; x++) {
+        console.log('X'.repeat(this.width));
+      }
+    }
+  }
+}
+
+module.exports = Rectangle;
