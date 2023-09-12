7dd2277c727f647a77d7f09e2cc53e95da4f440c
Biitopher <biitopher@gmail.com>
2023-09-12 08:15:31 -0700

    Task 10

--- 0x13-javascript_objects_scopes_closures/10-converter.js
+++ 0x13-javascript_objects_scopes_closures/10-converter.js
@@ -1,12 +1,5 @@
 #!/usr/bin/node
 
 exports.converter = function (base) {
-  return function convertToBase (num) {
-    if (num < base) {
-      return num.toString();
-    }
-    const remainder = num % base;
-    const quotient = Math.floor(num / base);
-    return convertToBase(quotient) + remainder.toString();
-  };
+  return num => num.toString(base);
 };
