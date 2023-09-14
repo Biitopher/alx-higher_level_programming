a1eb97eaf49c89fdf3d0e6db56b715d9179da749
Biitopher <biitopher@gmail.com>
2023-09-14 07:30:02 -0700

    Task 0

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python3
+
+
+import MySQLdb
+import sys
+
+
+def list_states(username, password, database_name):
+    try:
+        connection = MySQLdb.connect(
+            host='localhost',
+            user=username,
+            passwd=password,
+            db=database_name,
+            port=3306
+        )
+
+        cursor = connection.cursor()
+
+        cursor.execute("SELECT * FROM states ORDER BY states.id ASC")
+
+        states = cursor.fetchall()
+
+        for state in states:
+            print(state)
+
+        cursor.close()
+        connection.close()
+
+    except MySQLdb.Error as e:
+        print(f"Error: {e}")
+        sys.exit(1)
+
+
+if __name__ == "__main__":
+    if len(sys.argv) != 4:
+        print("Usage: python script.py <username> <password> <database_name>")
+        sys.exit(1)
+
+    username, password, database_name = sys.argv[1], sys.argv[2], sys.argv[3]
+    list_states(username, password, database_name)
