8585abd6182100fb762066311836ffdadd523c56
Biitopher <biitopher@gmail.com>
2023-09-15 06:09:27 -0700

    Task 1

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,11 +1,11 @@
-#!/usr/bin/pythoni3
+#!/usr/bin/python3
 """Script listing State names starting with N"""
 
 import MySQLdb
 import sys
 
 
-def list_states_with_n(username, password, database_name):
+def list_states_with_N(username, password, database_name):
     try:
         connection = MySQLdb.connect(
             host='localhost',
@@ -17,8 +17,7 @@ def list_states_with_n(username, password, database_name):
 
         cursor = connection.cursor()
 
-        cursor.execute("SELECT * FROM states WHERE name LIKE 'N%'\
-                ORDER BY states.id ASC")
+        cursor.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY states.id ASC")
 
         states = cursor.fetchall()
 
@@ -39,4 +38,4 @@ if __name__ == "__main__":
         sys.exit(1)
 
     username, password, database_name = sys.argv[1], sys.argv[2], sys.argv[3]
-    list_states_with_n(username, password, database_name)
+    list_states_with_N(username, password, database_name)
---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Script displaying all values in the states"""
+
+
+import sys
+import MySQLdb
+
+
+
