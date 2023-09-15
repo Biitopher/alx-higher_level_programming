4dd8c184c50210519f49fe9a837c76c3d4e50d3b
Biitopher <biitopher@gmail.com>
2023-09-15 04:34:41 -0700

    Task 0

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
+"""Script listing all states"""
 
 
 import sys
 import MySQLdb
+
 
 def list_states(username, password, database_name):
     """ Function to list states """
@@ -22,6 +24,8 @@ def list_states(username, password, database_name):
     except MySQLdb.Error as e:
         print("MySQL Error: ", e)
         sys.exit(1)
+
+
 if __name__ == "__main__":
     if len(sys.argv) != 4:
         print("Wrong format")
