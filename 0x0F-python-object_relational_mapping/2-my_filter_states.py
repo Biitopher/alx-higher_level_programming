439647176601d9a18dac3845deb9a865044e4784
Biitopher <biitopher@gmail.com>
2023-09-15 09:50:42 -0700

    Task 2

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -6,6 +6,35 @@ import sys
 import MySQLdb
 
 
+def search_states(username, password, database_name, state_name):
+    try:
+        db = MySQLdb.connect(
+                host='localhost',
+                port=3306,
+                user=username,
+                passwd=password,
+                db=database_name
+                )
+
+        cursor = db.cursor()
+
+        query = "SELECT * FROM states WHERE name = %s ORDER BY id ASC"
+
+        cursor.execute(query, (state_name,))
+
+        results = cursor.fetchall()
+
+        for row in results:
+            print(row)
+
+        cursor.close()
+        db.close()
+
+    except MySQLdb.Error as e:
+        print("MySQL Error:", e)
+        sys.exit(1)
+
+
 if __name__ == "__main__":
     if len(sys.argv) != 5:
         print("Usage: python \
@@ -13,5 +42,7 @@ if __name__ == "__main__":
         sys.exit(1)
 
     username, password, database_name, state_name = (sys.argv[1],
-    sys.argv[2], sys.argv[3], sys.argv[4])
+                                                     sys.argv[2],
+                                                     sys.argv[3],
+                                                     sys.argv[4])
     search_states(username, password, database_name, state_name)
