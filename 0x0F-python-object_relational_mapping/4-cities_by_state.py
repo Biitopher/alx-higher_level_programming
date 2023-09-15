c204f2ad6744adb9598e15960b7a09f7dd0fe4fd
Biitopher <biitopher@gmail.com>
2023-09-15 14:39:24 -0700

    Task 4

---
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+"""script that lists all cities from the database"""
+
+
+import MySQLdb
+import sys
+
+
+def list_cities(username, password, database):
+    try:
+        db = MySQLdb.connect(host='localhost', port=3306, user=username, passwd=password, db=database)
+
+        cursor = db.cursor()
+
+        query = "SELECT * FROM cities ORDER BY id ASC"
+
+        cursor.execute(query)
+
+        results = cursor.fetchall()
+
+        for row in results:
+            print(row)
+
+        cursor.close()
+        db.close()
+
+    except MySQLdb.Error as e:
+        print("MySQL Error:", e)
+        sys.exit(1)
+
+
+if __name__ == "__main__":
+    if len(sys.argv) != 4:
+        print("Usage: python script.py <username> <password> <database>")
+        sys.exit(1)
+
+    username, password, database = sys.argv[1], sys.argv[2], sys.argv[3]
+    list_cities(username, password, database)
