a40a522bb1bf1c65f0f9f9484b26c2ee110dfe73
Biitopher <biitopher@gmail.com>
2023-09-16 09:42:57 -0700

    Task 5

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -6,38 +6,11 @@ import MySQLdb
 import sys
 
 
-def list_cities_by_state(username, password, database, state_name):
-    try:
-        db = MySQLdb.connect(host='localhost',
-                             port=3306,
-                             user=username,
-                             passwd=password,
-                             db=database)
-        cursor = db.cursor()
-        query = "SELECT * FROM cities ORDER BY id ASC"
-        query = ("SELECT * FROM `cities` as `c` \
+if __name__ == "__main__":
+    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+    c = db.cursor()
+    c.execute("SELECT * FROM `cities` as `c` \
                 INNER JOIN `states` as `s` \
                    ON `c`.`state_id` = `s`.`id` \
                 ORDER BY `c`.`id`")
-        cursor.execute(query)
-        results = cursor.fetchall()
-        for row in results:
-            print(row)
-        cursor.close()
-        db.close()
-    except MySQLdb.Error as e:
-        print("MySQL Error:", e)
-        sys.exit(1)
-
-
-if __name__ == "__main__":
-    if len(sys.argv) != 4:
-        print("Usage: python \
-                script.py <username> <password> <database> <state_name>")
-        sys.exit(1)
-
-    (username, password, database, state_name) = (sys.argv[1],
-                                                    sys.argv[2],
-                                                    sys.argv[3],
-                                                    sys.argv[4])
-    list_cities_by_state(username, password, database, state_name)
+    print(", ".join([ct[2] for ct in c.fetchall() if ct[4] == sys.argv[3]]))
