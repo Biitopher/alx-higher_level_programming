62e9813d2642ba68375cd68182a243807d95692a
Biitopher <biitopher@gmail.com>
2023-09-16 16:41:08 -0700

    Task 5

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -6,38 +6,39 @@ import MySQLdb
 import sys
 
 
-def list_cities_by_state(username, password, database, state_name):
+def list_cities_by_state(username, password, database_name, state_name):
     try:
-        db = MySQLdb.connect(host='localhost',
-                             port=3306,
-                             user=username,
-                             passwd=password,
-                             db=database)
-        cursor = db.cursor()
-        query = "SELECT * FROM cities ORDER BY id ASC"
-        query = ("SELECT * FROM `cities` as `c` \
-                INNER JOIN `states` as `s` \
-                   ON `c`.`state_id` = `s`.`id` \
-                ORDER BY `c`.`id`")
-        cursor.execute(query)
-        results = cursor.fetchall()
-        for row in results:
-            print(row)
-        cursor.close()
-        db.close()
+        db = MySQLdb.connect(host="localhost", user=username,
+                             passwd=password, db=database_name, port=3306)
+        cur = db.cursor()
+
+        query = """SELECT cities.name
+                   FROM cities
+                   INNER JOIN states ON states.id=cities.state_id
+                   WHERE states.name=%s
+                   ORDER BY cities.id ASC"""
+
+        cur.execute(query, (state_name,))
+        rows = cur.fetchall()
+
+        for row in rows:
+            print(row[0])
+
     except MySQLdb.Error as e:
-        print("MySQL Error:", e)
-        sys.exit(1)
+        print(f"MySQL Error: {e}")
+    finally:
+        if db:
+            db.close()
 
 
 if __name__ == "__main__":
-    if len(sys.argv) != 4:
-        print("Usage: python \
-                script.py <username> <password> <database> <state_name>")
+    if len(sys.argv) != 5:
+        print("Usage: python script_name.py \
+                <username> <password> <database_name> <state_name>")
         sys.exit(1)
 
-    (username, password, database, state_name) = (sys.argv[1],
-                                                  sys.argv[2],
-                                                  sys.argv[3],
-                                                  sys.argv[4])
-    list_cities_by_state(username, password, database, state_name)
+    (username, password, database_name, state_name) = (sys.argv[1],
+                                                       sys.argv[2],
+                                                       sys.argv[3],
+                                                       sys.argv[4])
+    list_cities_by_state(username, password, database_name, state_name)
