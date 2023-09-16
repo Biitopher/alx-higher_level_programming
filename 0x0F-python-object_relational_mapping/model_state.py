f4181b1d109169550904ccc7066ab9b73126111d
Biitopher <biitopher@gmail.com>
2023-09-16 13:26:40 -0700

    Task 6

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -9,10 +9,12 @@ from sqlalchemy import create_engine
 Base = declarative_base()
 
 class State(Base):
+    """Defines state"""
     __tablename__ = 'states'
 
     id = Column(Integer, primary_key=True, nullable=False, autoincrement=True)
     name = Column(String(128), nullable=False)
+
 
 if __name__ == "__main__":
     db_url = "mysql://<username>:<password>@localhost:3306/<database_name>"
