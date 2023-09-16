6363d06fd753f5d93b84e5b51aba25fc7d898bed
Biitopher <biitopher@gmail.com>
2023-09-16 16:02:14 -0700

    Task 9

--- 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -9,19 +9,22 @@ from sqlalchemy.orm import sessionmaker
 
 if __name__ == "__main__":
     if len(sys.argv) != 4:
-        print("Usage: python script_name.py <username> <password> <database_name>")
+        print("Usage: python \
+                script_name.py <username> <password> <database_name>")
         sys.exit(1)
 
     username, password, database_name = sys.argv[1], sys.argv[2], sys.argv[3]
 
-    engine = create_engine(f'mysql+mysqldb://{username}:{password}@localhost:3306/{database_name}')
+    engine = create_engine(f'mysql+mysqldb://{username}:\
+            {password}@localhost:3306/{database_name}')
 
     Base.metadata.create_all(engine)
 
     Session = sessionmaker(bind=engine)
     session = Session()
 
-    results = session.query(State).filter(State.name.like('%a%')).order_by(State.id).all()
+    results = session.query(State)\
+        .filter(State.name.like('%a%')).order_by(State.id).all()
 
     for state in results:
         print(f"{state.id}: {state.name}")
