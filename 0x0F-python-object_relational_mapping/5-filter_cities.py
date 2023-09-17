28098647d864d38bf1aa543d5c7846fc78f62247
Biitopher <biitopher@gmail.com>
2023-09-17 06:46:26 -0700

    Task 5

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -6,39 +6,36 @@ import MySQLdb
 import sys
 
 
-def list_cities_by_state(username, password, database_name, state_name):
-    try:
-        db = MySQLdb.connect(host="localhost", user=username,
-                             passwd=password, db=database_name, port=3306)
-        cur = db.cursor()
-
-        query = """SELECT cities.name
-                   FROM cities
-                   INNER JOIN states ON states.id=cities.state_id
-                   WHERE states.name=%s
-                   ORDER BY cities.id ASC"""
-
-        cur.execute(query, (state_name,))
-        rows = cur.fetchall()
-
-        for row in rows:
-            print(row[0])
-
-    except MySQLdb.Error as e:
-        print(f"MySQL Error: {e}")
-    finally:
-        if db:
-            db.close()
-
-
 if __name__ == "__main__":
     if len(sys.argv) != 5:
-        print("Usage: python script_name.py \
-                <username> <password> <database_name> <state_name>")
+        print("Usage: python script.py <mysql_username> \
+                <mysql_password> <database_name> <state_name>")
         sys.exit(1)
 
-    (username, password, database_name, state_name) = (sys.argv[1],
-                                                       sys.argv[2],
-                                                       sys.argv[3],
-                                                       sys.argv[4])
-    list_cities_by_state(username, password, database_name, state_name)
+    mysql_username = sys.argv[1]
+    mysql_password = sys.argv[2]
+    database_name = sys.argv[3]
+    state_name = sys.argv[4]
+
+    db = MySQLdb.connect(host="localhost",
+                         port=3306,
+                         user=mysql_username,
+                         passwd=mysql_password,
+                         db=database_name)
+    cursor = db.cursor()
+
+    query = """
+        SELECT cities.id, cities.name, states.name
+        FROM cities
+        INNER JOIN states ON cities.state_id = states.id
+        WHERE states.name = %s
+        ORDER BY cities.id ASC
+    """
+    cursor.execute(query, (state_name,))
+
+    results = cursor.fetchall()
+    for row in results:
+        print(row)
+
+    cursor.close()
+    db.close()
