b6aa7c893de2f242c1b7800a5fdd3bd1305ee82d
Biitopher <biitopher@gmail.com>
2023-09-17 06:26:16 -0700

    Task 7

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -8,36 +8,25 @@ from model_state import Base, State
 import sys
 
 
-def list_states(username, password, database_name):
-    try:
-        db_url = f"mysql://{username}:\
-                {password}@localhost:3306/{database_name}"
-
-        engine = create_engine(db_url)
-
-        Session = sessionmaker(bind=engine)
-        session = Session()
-
-        states = session.query(State).order_by(State.id).all()
-
-        if states:
-            for state in states:
-                print(f"{state.id}: {state.name}")
-        else:
-            print("No states found in the database.")
-
-        session.close()
-
-    except Exception as e:
-        print(f"Error: {e}")
-        sys.exit(1)
-
-
 if __name__ == "__main__":
     if len(sys.argv) != 4:
         print("Usage: python \
-                script.py <username> <password> <database_name>")
-    else:
-        (username, password, database_name) = (sys.argv[1],
-                                               sys.argv[2], sys.argv[3])
-    list_states(username, password, database_name)
+                script.py <mysql_username> <mysql_password> <database_name>")
+        sys.exit(1)
+
+    mysql_username = sys.argv[1]
+    mysql_password = sys.argv[2]
+    database_name = sys.argv[3]
+
+    engine = create_engine(f'mysql+mysqldb://{mysql_username}:\
+            {mysql_password}@localhost:3306/{database_name}')
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    states = session.query(State).order_by(State.id).all()
+
+    for state in states:
+        print(f"{state.id}: {state.name}")
+
+    session.close()
