e217b63a3bac2a1a5b4cc73b2fb9fcda9d91101a
Biitopher <biitopher@gmail.com>
2023-09-17 05:47:36 -0700

    Task 12

---
+++ 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+"""script that changes the name of a State object from the database"""
+
+
+import sys
+from model_state import Base, State
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+if __name__ == "__main__":
+    if len(sys.argv) != 4:
+        print("Usage: python \
+                script.py <mysql_username> <mysql_password> <database_name>")
+        sys.exit(1)
+
+    mysql_username = sys.argv[1]
+    mysql_password = sys.argv[2]
+    database_name = sys.argv[3]
+
+    engine = create_engine(f'mysql+mysqldb://{mysql_username}:\
+            {mysql_password}@localhost:3306/{database_name}')
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    state_to_update = session.query(State).filter_by(id=2).first()
+
+    if state_to_update:
+        state_to_update.name = "New Mexico"
+
+        session.commit()
+        print(f"Updated State ID \
+                {state_to_update.id} to '{state_to_update.name}'")
+    else:
+        print("State with ID 2 not found in the database.")
