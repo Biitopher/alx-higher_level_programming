bc151ae48d4d562e61b11d79c273ef23dacc6597
Biitopher <biitopher@gmail.com>
2023-09-17 06:37:00 -0700

    Task 3

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -5,10 +5,30 @@
 import sys
 import MySQLdb
 
-
 if __name__ == "__main__":
-    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
-    c = db.cursor()
-    name = sys.argv[4].strip('"').strip("'")
-    c.execute("SELECT * FROM `states` WHERE `name` = '{}'".format(name))
-    [print(state) for state in c.fetchall()]
+    if len(sys.argv) != 5:
+        print("Usage: python script.py <mysql_username> \
+                <mysql_password> <database_name> <state_name>")
+        sys.exit(1)
+
+    mysql_username = sys.argv[1]
+    mysql_password = sys.argv[2]
+    database_name = sys.argv[3]
+    state_name = sys.argv[4]
+
+    db = MySQLdb.connect(host="localhost",
+                         port=3306,
+                         user=mysql_username,
+                         passwd=mysql_password,
+                         db=database_name)
+    cursor = db.cursor()
+
+    query = "SELECT * FROM states WHERE name = %s ORDER BY id ASC"
+    cursor.execute(query, (state_name,))
+
+    results = cursor.fetchall()
+    for row in results:
+        print(row)
+
+    cursor.close()
+    db.close()
