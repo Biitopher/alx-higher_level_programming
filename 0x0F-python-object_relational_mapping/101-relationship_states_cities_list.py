418b2a312faced479c04c0fccda1f329c8bc34ce
Biitopher <biitopher@gmail.com>
2023-09-17 18:47:30 -0700

    Task 16

--- 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
+++ 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
@@ -4,32 +4,29 @@
 import sys
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from relationship_state import Base, State
 from relationship_city import City
+from relationship_state import Base, State
 
+if __name__ == "__main__":
+    if len(sys.argv) != 4:
+        print("Usage: python \
+                script.py <mysql_username> <mysql_password> <database_name>")
+        sys.exit(1)
 
-def list_states_cities(username, password, db_name):
-    """List State and City objects from the database"""
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.
-                           format(username, password, db_name),
-                           pool_pre_ping=True)
+    mysql_username, mysql_password, database_name = (sys.argv[1],
+                                                     sys.argv[2], sys.argv[3])
+
+    engine = create_engine(f'mysql+mysqldb://{mysql_username}:\
+            {mysql_password}@localhost:3306/{database_name}')
 
     Session = sessionmaker(bind=engine)
     session = Session()
 
-    query = session.query(State, City).filter(State.id == City.state_id).
-    order_by(State.id, City.id).all()
+    query = session.query(State).order_by(State.id).all()
 
-    for state, city in query:
-        print("{}: {}".format(state.name, city.name))
+    for state in query:
+        print(f"{state.id}: {state.name}")
+        for city in state.cities:
+            print(f"    {city.id}: {city.name}")
 
     session.close()
-
-
-if __name__ == "__main__":
-    if len(sys.argv) != 4:
-        print("Usage: {} <username> <password> <db_name>".format(sys.argv[0]))
-        sys.exit(1)
-
-    username, password, db_name = sys.argv[1], sys.argv[2], sys.argv[3]
-    list_states_cities(username, password, db_name)
