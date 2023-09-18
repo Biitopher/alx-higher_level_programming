85d0b13e82acd13208cdbca75a99cd8952179e5d
Biitopher <biitopher@gmail.com>
2023-09-17 17:42:38 -0700

    Task 5

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -6,36 +6,24 @@ import MySQLdb
 import sys
 
 
-if __name__ == "__main__":
-    if len(sys.argv) != 5:
-        print("Usage: python script.py <mysql_username> \
-                <mysql_password> <database_name> <state_name>")
+def filter_cities(username, password, database_name, state_name):
+    """ Function to filter cities by state """
+    try:
+        db = MySQLdb.connect(host='localhost',
+                             port=3306,
+                             user=username,
+                             passwd=password,
+                             db=database_name)
+        c = db.cursor()
+        query = ("SELECT cities.name FROM cities "
+                 "INNER JOIN states ON cities.state_id = states.id "
+                 "WHERE states.name = %s "
+                 "ORDER BY cities.id ASC")
+        c.execute(query, (state_name,))
+        results = cursor.fetchall()
+        print(results)
+        city_names = ', '.join(city[0] for city in cities)
+        print(city_names)
+    except MySQLdb.Error as e:
+        print("MySQL Error: ", e)
         sys.exit(1)
-
-    mysql_username = sys.argv[1]
-    mysql_password = sys.argv[2]
-    database_name = sys.argv[3]
-    state_name = sys.argv[4]
-
-    db = MySQLdb.connect(host="localhost",
-                         port=3306,
-                         user=mysql_username,
-                         passwd=mysql_password,
-                         db=database_name)
-    cursor = db.cursor()
-
-    query = """
-        SELECT cities.id, cities.name, states.name
-        FROM cities
-        INNER JOIN states ON cities.state_id = states.id
-        WHERE states.name = %s
-        ORDER BY cities.id ASC
-    """
-    cursor.execute(query, (state_name,))
-
-    results = cursor.fetchall()
-    for row in results:
-        print(row)
-
-    cursor.close()
-    db.close()
