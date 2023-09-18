efd968ab4057455e2af8de9ec7f73bc0d4c8ab87
Biitopher <biitopher@gmail.com>
2023-09-17 17:25:33 -0700

    Task 16

--- 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
+++ 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
@@ -1,33 +1,35 @@
 #!/usr/bin/python3
-"""script listing State objects, and corresponding City objects in database"""
-
+"""Script to list State and City objects from the database using SQLAlchemy"""
 
 import sys
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from model_state import Base, State
-from model_city import City
+from relationship_state import Base, State
+from relationship_city import City
 
 
-def list_states_and_cities(username, password, database):
-    engine = create_engine(f'mysql://{username}:\
-            {password}@localhost:3306/{database}')
+def list_states_cities(username, password, db_name):
+    """List State and City objects from the database"""
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.
+                           format(username, password, db_name),
+                           pool_pre_ping=True)
 
     Session = sessionmaker(bind=engine)
     session = Session()
 
-    states_and_cities = (session.query(State, City)
-                         .join(City).order_by(State.id, City.id).all())
+    query = session.query(State, City).filter(State.id == City.state_id).
+    order_by(State.id, City.id).all()
+
+    for state, city in query:
+        print("{}: {}".format(state.name, city.name))
 
     session.close()
-
-    for state, city in states_and_cities:
-        print(f"{state.id}: {state.name}\n\t{city.id}: {city.name}")
 
 
 if __name__ == "__main__":
     if len(sys.argv) != 4:
-        print("Usage: python script.py <username> <password> <database>")
-    else:
-        username, password, database = sys.argv[1], sys.argv[2], sys.argv[3]
-        list_states_and_cities(username, password, database)
+        print("Usage: {} <username> <password> <db_name>".format(sys.argv[0]))
+        sys.exit(1)
+
+    username, password, db_name = sys.argv[1], sys.argv[2], sys.argv[3]
+    list_states_cities(username, password, db_name)
