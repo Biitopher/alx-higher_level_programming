29ad2db37b380e507e92014b55053f51c90aa232
Biitopher <biitopher@gmail.com>
2023-09-28 09:25:47 -0700

    Task 8

--- 0x10-python-network_0/101-post_json.sh
+++ 0x10-python-network_0/101-post_json.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #Sends a JSON POST request to a URL passed as the first argument
-curl -s -X POST -d @"$json_file" -H "Content-Type: application/json" "$1"
+curl -s -X POST $1 -H "Content-Type: application/json" -d @$2 -L
