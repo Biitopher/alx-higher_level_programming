b9968f9948fd4a8f72190918ce444c9b70b00fc3
Biitopher <biitopher@gmail.com>
2023-09-28 07:49:20 -0700

    Task 5

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Script that takes in URL sends POST request to passed URL
-curl -sX POST $1 -d "email=hr@holbertonschool.com&subject=I will always be here for PLD" -L
+curl -s -X POST $1 -d "email=test@gmail.com&subject=I will always be here for PLD" -L
