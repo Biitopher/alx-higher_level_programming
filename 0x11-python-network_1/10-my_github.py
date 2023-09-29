03bb7f44d46d104da05e50fe4397ea1ee9c55131
Biitopher <biitopher@gmail.com>
2023-09-29 06:37:11 -0700

    Task 9

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -4,28 +4,21 @@
 import requests
 import sys
 
-if len(sys.argv) != 3:
-    print("Usage: python script.py <username> <personal_access_token>")
-    sys.exit(1)
-
 username = sys.argv[1]
 access_token = sys.argv[2]
 
-url = "https://api.github.com/user"
 headers = {
-    "Authorization": f"Basic {username}:{access_token}",
+    "Authorization": f"Basic {username}:{access_token}"
 }
 
-try:
-    response = requests.get(url, headers=headers)
-    response.raise_for_status()
+url = "https://api.github.com/user"
+response = requests.get(url, headers=headers)
 
-    data = response.json()
-    user_id = data.get("id")
-
-    if user_id:
-        print(f"Your GitHub user ID is: {user_id}")
-    else:
-        print("User ID not found in the response.")
-except requests.exceptions.RequestException as e:
-    print(f"Error: {e}")
+if response.status_code == 200:
+    user_data = response.json()
+    user_id = user_data["id"]
+    print(f"Your GitHub user ID is: {user_id}")
+elif response.status_code == 401:
+    print("Authentication failed. Please check your username and access token.")
+else:
+    print(f"An error occurred. Status code: {response.status_code}")
