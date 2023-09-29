9dbf6190135fe281acfecff19b83b4af47261815
Biitopher <biitopher@gmail.com>
2023-09-29 07:21:39 -0700

    Task 1

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -6,6 +6,6 @@ import sys
 
 if __name__ == "__main__":
     """Request to the URL and displays value of variable"""
-with urllib.request.urlopen(argv[1]) as response:
+    with urllib.request.urlopen(argv[1]) as response:
         x_request_id = response.info().get('X-Request-Id')
         print(x_request_id)
