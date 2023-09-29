736e2cc9cf9ed6aa86f0f2ab58d581d469f9ee1b
Biitopher <biitopher@gmail.com>
2023-09-29 04:27:33 -0700

    Task 1

---
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""Sends request to URL and displays value of ID"""
+
+import urllib.request
+import sys
+
+if len(sys.argv) != 2:
+    print("Usage: python script.py <URL>")
+    sys.exit(1)
+
+url = sys.argv[1]
+
+try:
+    with urllib.request.urlopen(url) as response:
+        x_request_id = response.getheader("X-Request-Id")
+        if x_request_id:
+            print(x_request_id)
+        else:
+            print("X-Request-Id not found in response headers.")
+except Exception as e:
+    print(f"Error: {e}")
