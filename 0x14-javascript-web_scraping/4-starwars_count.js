929959e2487a3e2c0918e0dc9b79b3f36ba1d8c2
biitopher <biitopher@github.com>
2023-10-24 14:56:06 -0700

    Task 4

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,32 +1,12 @@
 #!/usr/bin/node
-
 const request = require('request');
-
-if (process.argv.length !== 4) {
-  console.log('Usage: node starwars_title.js <movie_id> <episode_number>');
-  process.exit(1);
-}
-
-const movieId = process.argv[2];
-const episodeNumberToMatch = parseInt(process.argv[3], 10);
-const apiUrl = `https://swapi-api.alx-tools.com/api/films/${movieId}`;
-
-request(apiUrl, (error, response, body) => {
-  if (error) {
-    console.error(error);
-  } else if (response.statusCode !== 200) {
-    console.error(response.statusCode);
-  } else {
-    try {
-      const movieData = JSON.parse(body);
-      if (movieData.episode_id === episodeNumberToMatch) {
-        console.log(movieData.title);
-      } else {
-        console.log(`No Star Wars movie found with Episode Number ${episodeNumberToMatch}`);
-      }
-    } catch (parseError) {
-      console.error(parseError);
-    }
+request(process.argv[2], function (error, response, body) {
+  if (!error) {
+    const data = JSON.parse(body).data;
+    console.log(data.reduce((count, movie) => {
+      return movie.characters.find((character) => character.endsWith('/18/'))
+        ? times + 1
+        : times;
+    }, 0));
   }
 });
-
