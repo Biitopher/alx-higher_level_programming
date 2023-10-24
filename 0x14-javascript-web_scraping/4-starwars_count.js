351d7edcce1d90d9f004b52e85f4a90cea0c646c
biitopher <biitopher@github.com>
2023-10-24 08:34:17 -0700

    Task 4

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,21 +1,13 @@
 #!/usr/bin/node
 const request = require('request');
-
-if (process.argv.length !== 3) {
-  console.log('Usage: ./wedge_antilles_movies.js <API_URL>');
-} else {
-  const apiUrl = process.argv[2];
-
-  request(apiUrl, (error, response, body) => {
+const apiUrl = process.argv[2];
+request(apiUrl, (error, response, body) => {
     if (!error && response.statusCode === 200) {
-      const data = JSON.parse(body);
-      const wedgeAntillesMovies = data.results.filter((movie) =>
-        movie.characters.includes('https://swapi-api.alx-tools.com/api/people/18/')
-      );
-
-      console.log(`Number of movies with Wedge Antilles: ${wedgeAntillesMovies.length}`);
-    } else {
-      console.error(`Error: ${error || 'API request failed'}`);
-    }
-  });
-}
+	const data = JSON.parse(body);
+	console.log(results.reduce((count, movie) => {
+      		return movie.characters.find((character) => character.endsWith('/18/'))
+        ? count + 1
+        : count;
+    }, 0));
+  }
+});
