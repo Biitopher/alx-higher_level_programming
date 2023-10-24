c3a06ba9409f181709787270fe9f7b2f898b62fa
biitopher <biitopher@github.com>
2023-10-24 11:34:02 -0700

    Task 4

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,23 +1,9 @@
 #!/usr/bin/node
 const request = require('request');
 const starWarsApi = process.argv[2];
-let times = 0;
 
-request(starWarsApi, function (_error, response, body) {
-  body = JSON.parse(body).data;
-
-  for (let a = 0; a < body.length; ++a) {
-    const characters = body[a].characters;
-
-    for (let j = 0; j < characters.length; ++j) {
-      const character = characters[j];
-      const characterId = character.split('/')[5];
-
-      if (characterId === '18') {
-        times += 1;
-      }
-    }
-  }
-
-  console.log(times);
+request(starWarsApi, function (error, response, body) {
+	if (error) console.log(error);
+	const times = character.split('/people/18').length - 1;
+	console.log(times);
 });
