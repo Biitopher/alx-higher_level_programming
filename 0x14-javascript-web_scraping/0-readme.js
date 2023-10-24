f706960b1ff88c56ea5fec72eb5e58a8291fa0b3
biitopher <biitopher@github.com>
2023-10-24 03:14:39 -0700

    Task 0

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -2,19 +2,10 @@
 
 const fs = require('fs');
 
-function readAndPrintFileContent(filePath) {
-    fs.readFile(filePath, 'utf-8', (err, data) => {
-        if (err) {
-            console.error(`An error occurred: ${err}`);
-        } else {
-            console.log(data);
-        }
-    });
-}
-
-if (process.argv.length !== 3) {
-    console.log("Usage: node read_file.js <file_path>");
-} else {
-    const filePath = process.argv[2];
-    readAndPrintFileContent(filePath);
-}
+fs.readPrint_file(process.argv[2], 'utf8', function (error, data) {
+  if (error) {
+    console.log(error);
+  } else {
+    process.stdout.write(data);
+  }
+});
