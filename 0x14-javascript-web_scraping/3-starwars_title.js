1cf1b5e3ec3b3792c9479ab6de83580509e8bde3
biitopher <biitopher@github.com>
2023-10-24 05:23:29 -0700

    Task 3

---
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -0,0 +1,6 @@
+#!/usr/bin/node
+const request = require('request');
+let url = 'http://swapi.co/api/films/' + process.argv[2];
+request(url, function (err, response, body) {
+  console.log(err || JSON.parse(body).title);
+});
